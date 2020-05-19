2311c55c5557bdb2da47eadd9da833f05931e052
hkleindl <hkleindl@gmail.com>
2020-05-19 17:09:51 -0400

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_210613) do
+ActiveRecord::Schema.define(version: 2020_05_19_210750) do
 
   create_table "items", force: :cascade do |t|
     t.string "content"
     t.boolean "is_complete"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "list_id"
   end
 
   create_table "lists", force: :cascade do |t|
