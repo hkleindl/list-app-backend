611d28ec55dfc998885ebf566363f3ccb4efb8ba
hkleindl <hkleindl@gmail.com>
2020-05-19 17:07:11 -0400

    Add user_id to Lists model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_204011) do
+ActiveRecord::Schema.define(version: 2020_05_19_210613) do
 
   create_table "items", force: :cascade do |t|
     t.string "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_204011) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
