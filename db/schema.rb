d65e04d1368ca9a5f7ea5426e4d7a5c76e779994
hkleindl <hkleindl@gmail.com>
2020-05-19 16:41:01 -0400

    Add List and Item scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_201730) do
+ActiveRecord::Schema.define(version: 2020_05_19_204011) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "content"
+    t.boolean "is_complete"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
