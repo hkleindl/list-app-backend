255f674006a2df873cc374f313779c42ac5a625e
hkleindl <hkleindl@gmail.com>
2020-05-20 15:48:15 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    username = "#{Faker::Color.unique.color_name}_#{Faker::Creature::Animal.unique.name}"
+    username = username.tr(" ", "_")
+    User.create(username: username, password: "password")
+end
+
+User.all.each do |user|
+    3.times do
+        list_name = Faker::Hipster.word.capitalize
+        user.lists.create(name: list_name)
+    end
+end
+
+List.all.each do |list|
+    6.times do
+        content = Faker::Hipster.sentence
+        list.items.create(content: content)
+    end
+end
