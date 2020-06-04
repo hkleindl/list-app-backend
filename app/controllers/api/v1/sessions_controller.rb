bd63771fe452986d9c7d551da2c2f824dfee1d66
hkleindl <hkleindl@gmail.com>
2020-06-04 16:06:29 -0400

    remove session from query

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-        @user = User.find_by(username: params[:session][:username])
-        if @user && @user.authenticate(params[:session][:password])
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             render json: @user
         else
